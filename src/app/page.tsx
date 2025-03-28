bca490db60a820891ee6f7eb6eefed9f58d503a0
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-28 23:04:22 +0300

    Fix background image URL format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,34 @@ export default function Home() {
     <main className="min-h-screen bg-black relative">
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-        style={{
-          backgroundImage: 'url("https://imgur.com/a/hAnfYlP")'
-        }}
+        style={{ backgroundImage: 'url(https://i.imgur.com/hAnfYlP.png)' }}
       />
+      <div className="relative z-10">
+        <Header />
+        <ImageSlider />
+        <div className="grid grid-cols-2 gap-4 p-4">
+          <ServiceCard
+            title="Дизайн логотипа"
+            image="https://i.imgur.com/68Dupqy.png"
+            price="от 100$"
+          />
+          <ServiceCard
+            title="Дизайн сайта"
+            image="https://i.imgur.com/68Dupqy.png"
+            price="от 200$"
+          />
+          <ServiceCard
+            title="Дизайн приложения"
+            image="https://i.imgur.com/68Dupqy.png"
+            price="от 300$"
+          />
+          <ServiceCard
+            title="Дизайн баннера"
+            image="https://i.imgur.com/68Dupqy.png"
+            price="от 50$"
+          />
+        </div>
+      </div>
     </main>
   );
 }
