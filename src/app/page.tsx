3c7c716d891db1b39366a934cd1c7085ec19d609
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-28 23:11:13 +0300

    Update background image format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import Header from "@/components/Header";
+import ImageSlider from "@/components/ImageSlider";
+import ServiceCard from "@/components/ServiceCard";
+
+const services = [
+  { image: "https://i.imgur.com/bYR0xw8.png" },
+  { image: "https://i.imgur.com/r3ojTdM.png" },
+  { image: "https://i.imgur.com/Swyq8NY.png" },
+  { image: "https://i.imgur.com/q18NojN.png" }
+];
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black relative">
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: 'url(https://i.imgur.com/hAnfYlP.png)' }}
+        style={{
+          backgroundImage: 'url("https://i.imgur.com/S6NXHjG.png")'
+        }}
       />
     </main>
   );
