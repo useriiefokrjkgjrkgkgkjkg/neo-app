54c4e29b0ce36ff0a99671cbefe0f12f3222e74f
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-29 19:32:16 +0300

    Fix background image sizing for different devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,13 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black relative animate-fadeIn">
       <div 
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: 'url(https://i.imgur.com/vTpUuaK.png)' }}
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat h-screen w-screen max-w-screen-sm mx-auto"
+        style={{ 
+          backgroundImage: 'url(https://i.imgur.com/vTpUuaK.png)',
+          backgroundSize: '100% 100%',
+          backgroundPosition: 'center',
+          maxHeight: '926px' // высота iPhone 13
+        }}
       />
       <Navigation />
     </main>
