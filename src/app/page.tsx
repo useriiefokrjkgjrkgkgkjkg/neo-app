dd56bae05de0398b4b1818bc668662bd10d0832f
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-29 19:43:06 +0300

    Add responsive styles for different devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,9 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black relative animate-fadeIn flex flex-col">
       <div 
-        className="flex-1 relative"
+        className="background-container"
         style={{ 
-          backgroundImage: 'url(https://i.imgur.com/vTpUuaK.png)',
-          backgroundSize: 'contain',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          minHeight: '100vh',
-          width: '100%',
-          maxWidth: '100%'
+          backgroundImage: 'url(https://i.imgur.com/vTpUuaK.png)'
         }}
       />
       <Navigation />
