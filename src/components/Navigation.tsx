ac739d9b0c4b617df4d9511605d08a411cf08197
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-29 19:28:40 +0300

    Add spacing between navigation buttons

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,24 +5,24 @@ import Link from 'next/link';
 
 export default function Navigation() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-[#2D1E5A] rounded-t-[30px] px-8 py-6 flex justify-between items-center max-w-screen-sm mx-auto">
-      <Link href="/" className="flex flex-col items-center gap-2">
+    <nav className="fixed bottom-0 left-0 right-0 bg-[#2D1E5A] rounded-t-[30px] px-4 py-6 flex justify-between items-center max-w-screen-sm mx-auto">
+      <Link href="/" className="flex flex-col items-center gap-2 min-w-[70px]">
         <Image src="https://i.imgur.com/nkipwhd.png" alt="Главная" width={32} height={32} />
         <span className="text-white text-sm">Главная</span>
       </Link>
-      <Link href="/profile" className="flex flex-col items-center gap-2">
+      <Link href="/profile" className="flex flex-col items-center gap-2 min-w-[70px]">
         <Image src="https://i.imgur.com/MIT4jtp.png" alt="Профиль" width={32} height={32} />
         <span className="text-white text-sm">Профиль</span>
       </Link>
-      <Link href="/order" className="flex flex-col items-center gap-2">
+      <Link href="/order" className="flex flex-col items-center gap-2 min-w-[70px]">
         <Image src="https://i.imgur.com/fbU8tYW.png" alt="Заказать" width={32} height={32} />
         <span className="text-white text-sm">Заказать</span>
       </Link>
-      <Link href="/history" className="flex flex-col items-center gap-2">
+      <Link href="/history" className="flex flex-col items-center gap-2 min-w-[70px]">
         <Image src="https://i.imgur.com/oeFMDYZ.png" alt="История" width={32} height={32} />
         <span className="text-white text-sm">История</span>
       </Link>
-      <Link href="/support" className="flex flex-col items-center gap-2">
+      <Link href="/support" className="flex flex-col items-center gap-2 min-w-[70px]">
         <Image src="https://i.imgur.com/n0VBiKb.png" alt="Поддержка" width={32} height={32} />
         <span className="text-white text-sm">Поддержка</span>
       </Link>
