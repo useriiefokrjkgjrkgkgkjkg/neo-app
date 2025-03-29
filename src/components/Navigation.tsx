476386d76d11ea0e7080ccdb05b383ec9d943baa
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-29 19:25:01 +0300

    Increase navigation bar size

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,26 +5,26 @@ import Link from 'next/link';
 
 export default function Navigation() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-[#2D1E5A] rounded-t-[30px] px-6 py-5 flex justify-between items-center max-w-screen-sm mx-auto">
+    <nav className="fixed bottom-0 left-0 right-0 bg-[#2D1E5A] rounded-t-[30px] px-8 py-6 flex justify-between items-center max-w-screen-sm mx-auto">
       <Link href="/" className="flex flex-col items-center gap-2">
-        <Image src="https://i.imgur.com/nkipwhd.png" alt="Главная" width={28} height={28} />
-        <span className="text-white text-xs">Главная</span>
+        <Image src="https://i.imgur.com/nkipwhd.png" alt="Главная" width={32} height={32} />
+        <span className="text-white text-sm">Главная</span>
       </Link>
       <Link href="/profile" className="flex flex-col items-center gap-2">
-        <Image src="https://i.imgur.com/MIT4jtp.png" alt="Профиль" width={28} height={28} />
-        <span className="text-white text-xs">Профиль</span>
+        <Image src="https://i.imgur.com/MIT4jtp.png" alt="Профиль" width={32} height={32} />
+        <span className="text-white text-sm">Профиль</span>
       </Link>
       <Link href="/order" className="flex flex-col items-center gap-2">
-        <Image src="https://i.imgur.com/fbU8tYW.png" alt="Заказать" width={28} height={28} />
-        <span className="text-white text-xs">Заказать</span>
+        <Image src="https://i.imgur.com/fbU8tYW.png" alt="Заказать" width={32} height={32} />
+        <span className="text-white text-sm">Заказать</span>
       </Link>
       <Link href="/history" className="flex flex-col items-center gap-2">
-        <Image src="https://i.imgur.com/oeFMDYZ.png" alt="История" width={28} height={28} />
-        <span className="text-white text-xs">История</span>
+        <Image src="https://i.imgur.com/oeFMDYZ.png" alt="История" width={32} height={32} />
+        <span className="text-white text-sm">История</span>
       </Link>
       <Link href="/support" className="flex flex-col items-center gap-2">
-        <Image src="https://i.imgur.com/n0VBiKb.png" alt="Поддержка" width={28} height={28} />
-        <span className="text-white text-xs">Поддержка</span>
+        <Image src="https://i.imgur.com/n0VBiKb.png" alt="Поддержка" width={32} height={32} />
+        <span className="text-white text-sm">Поддержка</span>
       </Link>
     </nav>
   );
