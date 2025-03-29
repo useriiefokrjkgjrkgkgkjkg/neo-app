846369a07a6011102bcfca7474e79c4dea3b1267
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-29 18:56:36 +0300

    Clean up unused imports and update image URL format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
-import Header from "@/components/Header";
-import ImageSlider from "@/components/ImageSlider";
-import ServiceCard from "@/components/ServiceCard";
-
-const services = [
-  { image: "https://imgur.com/a/cdRqJ2v" },
-  { image: "https://imgur.com/a/cdRqJ2v" },
-  { image: "https://imgur.com/a/cdRqJ2v" },
-  { image: "https://imgur.com/a/cdRqJ2v" }
-];
-
 export default function Home() {
   return (
     <main className="min-h-screen bg-black relative">
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: 'url(https://i.imgur.com/9wZbroC.png)' }}
+        style={{ backgroundImage: 'url(https://i.imgur.com/vTpUuaK.png)' }}
       />
     </main>
   );
